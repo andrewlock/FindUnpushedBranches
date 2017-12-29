e5214dc00e2dc68430f263e579bd2527408cd31e
Andrew Lock <andrewlock.net@gmail.com>
2017-12-29 23:47:55 +0000

    Add initial implemntation

--- FindUnpushedBranches/Program.cs
+++ FindUnpushedBranches/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,80 @@ namespace FindUnpushedBranches
 {
     class Program
     {
+        static string[] SkipDirectories = { "node_modules" };
         static void Main(string[] args)
         {
+            var baseDirectory = args.Length == 0
+                ? Directory.GetCurrentDirectory()
+                : args[0];
+
+            if (!Directory.Exists(baseDirectory))
+            {
+                Console.WriteLine("Unable to search folders");
+                Console.WriteLine($"The directory '{baseDirectory}' does not exist");
+            }
+
+            Console.WriteLine($"Searching {baseDirectory} for Git repos");
+
+            FindGitFolders(baseDirectory);
+        }
+
+        private static void FindGitFolders(string baseDirectory, int level = 0)
+        {
+            var prefix = new string(' ', level);
+            var currentDirectory = Path.GetFileName(baseDirectory);
+
+            if (SkipDirectories.Contains(currentDirectory, StringComparer.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            Console.Write(prefix);
+            Console.Write(currentDirectory);
+            Console.WriteLine("/");
+
+            var directories = Directory.GetDirectories(baseDirectory);
+            var isGitRepo = directories
+                .Select(dir => Path.GetFileName(dir))
+                .Any(dir => string.Equals(".git", dir, StringComparison.OrdinalIgnoreCase));
+
+            if (isGitRepo)
+            {
+                PrintUnpushedBranches(baseDirectory, prefix);
+            }
+            else
+            {
+                foreach (var directory in directories)
+                {
+                    FindGitFolders(directory, level + 1);
+                }
+            }
+        }
+
+        private static void PrintUnpushedBranches(string directory, string prefix)
+        {
+            var previousColour = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = "log --branches --not --remotes --simplify-by-decoration --decorate --oneline",
+                    WorkingDirectory = directory,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                Console.Write(prefix);
+                Console.WriteLine(process.StandardOutput.ReadLine());
+            }
+
+            Console.ForegroundColor = previousColour;
         }
     }
 }
